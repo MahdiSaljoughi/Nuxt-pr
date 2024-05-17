a0a03838dd02b72bb280a3b5d69a4edc9003afb0
Mahdi <saljooghimahdii@gmail.com>
2024-05-17 14:28:06 +0330

    posterMobile

--- components/slider/slider.vue
+++ components/slider/slider.vue
@@ -1,41 +1,46 @@
 <template>
-    <div>
-        <swiper
-          :spaceBetween="30"
-          :centeredSlides="true"
-          :autoplay="{
-            delay: 5000,
-            disableOnInteraction: false,
-          }"
-          :pagination="{
-            clickable: true,
-          }"
-          :navigation="true"
-          :modules="modules"
-          id="slider"
-        >
-          <swiper-slide>
-            <img src="/p1.jpg" alt="p1" srcset="" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="/p2.jpg" alt="p2" srcset="" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="/p3.jpg" alt="p3" srcset="" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="/pgif.gif" alt="pgif" srcset="" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="/pkart.webp" alt="pkart" srcset="" />
-          </swiper-slide>
-        </swiper>
-    </div>
+  <div>
+    <swiper
+      :spaceBetween="30"
+      :centeredSlides="true"
+      :autoplay="{
+        delay: 5000,
+        disableOnInteraction: false,
+      }"
+      :pagination="{
+        clickable: true,
+      }"
+      :navigation="true"
+      :modules="modules"
+      id="slider"
+    >
+      <swiper-slide>
+        <!-- <img class="hidden md:block" src="/p1.jpg" alt="p1" srcset="" /> -->
+        <img class="block md:hidden" src="/public/posterMobileTcl.jpg" alt="" srcset="" />
+      </swiper-slide>
+      <swiper-slide>
+        <!-- <img class="hidden md:block" src="/p2.jpg" alt="p2" srcset="" /> -->
+        <img class="block md:hidden" src="/public/posterMobileHonor.webp" alt="" srcset="" />
+      </swiper-slide>
+      <swiper-slide>
+        <!-- <img class="hidden md:block" src="/p3.jpg" alt="p3" srcset="" /> -->
+        <img class="block md:hidden" src="/posterMobileKart.webp" alt="" srcset="" />
+      </swiper-slide>
+      <swiper-slide>
+        <!-- <img class="hidden md:block" src="/pgif.gif" alt="pgif" srcset="" /> -->
+        <img class="block md:hidden" src="/posterMobileSaat.jpg" alt="" srcset="" />
+      </swiper-slide>
+      <swiper-slide>
+        <!-- <img class="hidden md:block" src="/pkart.webp" alt="pkart" srcset="" /> -->
+        <img class="block md:hidden" src="/posterMobileUltra.jpg" alt="" srcset="" />
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
 
 <style>
 #slider {
-    z-index: 20;
+  z-index: 20;
 }
 </style>
 
