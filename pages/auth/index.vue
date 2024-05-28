8477e12f9e0d1b6c79f2552ecb4eb2a399378007
Mahdi <saljooghimahdii@gmail.com>
2024-05-28 18:28:41 +0330

    yup

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -5,38 +5,47 @@
   <div
     class="mt-[200px] md:mt-[300px] md:mb-[50px] flex justify-center items-center"
   >
-    <img src="/public/mobit.svg" alt="logo" style="margin-bottom: 20px;" class="w-[140px] md:w-[125px] z-50" />
+    <img
+      src="/public/mobit.svg"
+      alt="logo"
+      style="margin-bottom: 20px"
+      class="w-[140px] md:w-[125px] z-50"
+    />
   </div>
   <div
     id="app"
     class="text-center md:shadow-[0px_0px_40px_-10px_rgba(0,0,0,0.2)] w-[400px] mx-auto rounded-[24px] py-5 z-40"
   >
-    <Form action="/" method="POST" class="flex flex-col gap-y-5">
-      <Field
+    <Form method="POST" @submit="onSubmit" class="flex flex-col gap-y-5">
+      <input
+        v-model="username"
+        v-bind="usernameAttrs"
         name="username"
         type="text"
-        :rules="validateUserName"
-        class="mx-auto outline-none focus:ring-2 ring-blue-500 bg-gray-100 w-[90%] h-[45px] rounded-[16px] placeholder:text-[12px] px-4"
+        class="user mx-auto outline-none focus:ring-2 ring-blue-500 bg-gray-100 w-[90%] h-[45px] rounded-[16px] placeholder:text-[12px] px-4"
         placeholder="نام کاربری"
       />
-      <ErrorMessage name="username" />
-      <Field
+      <span>{{ errors.username }}</span>
+      <input
+        v-model="email"
+        v-bind="emailAttrs"
         name="email"
         type="email"
-        :rules="validateEmail"
         class="mx-auto outline-none focus:ring-2 ring-blue-500 bg-gray-100 w-[90%] h-[45px] rounded-[16px] placeholder:text-[12px] px-4"
         placeholder="ایمیل"
       />
-      <ErrorMessage name="email" />
-      <Field
+      <span>{{ errors.email }}</span>
+      <input
+        v-model="password"
+        v-bind="passwordAttrs"
         name="password"
         type="password"
-        :rules="validatePassword"
         class="mx-auto outline-none focus:ring-2 ring-blue-500 bg-gray-100 w-[90%] h-[45px] rounded-[16px] placeholder:text-[12px] px-4"
         placeholder="پسورد"
       />
-      <ErrorMessage name="password" />
-      <button type="submit"
+      <span>{{ errors.password }}</span>
+      <button
+        type="submit"
         class="mx-auto bg-gray-100 w-[90%] h-[45px] rounded-[16px] text-[12px] font-bold text-gray-400 focus:bg-blue-600 focus:text-white"
       >
         ثبت نام
@@ -44,60 +53,46 @@
     </Form>
   </div>
   <div>
-    <img src="/public/mfoter.svg" class="fixed bottom-0 w-full md:hidden" alt="" />
+    <img
+      src="/public/mfoter.svg"
+      class="fixed bottom-0 w-full md:hidden"
+      alt=""
+    />
   </div>
 </template>
+
 <style scoped>
 span {
   color: red;
-  background-color: rgba(255, 0, 0, 0.102);
+  /* background-color: rgba(255, 0, 0, 0.102);
   margin-left: 120px;
   margin-right: 120px;
   margin-top: -8px;
   padding: 8px;
+  border-radius: 16px; */
   font-size: 12.8px;
-  border-radius: 16px;
 }
 </style>
-<script>
-import { Form, Field, ErrorMessage } from "vee-validate";
-export default {
-  components: {
-    Form,
-    Field,
-    ErrorMessage,
-  },
-  methods: {
-    validateUserName(value) {
-      if (!value) {
-        return "این فیلد الزامی می باشد !";
-      }
-      const regex = /^[a-zA-Z]{3,10}/;
-      if (!regex.test(value)) {
-        return "نام کاربری معتبر نیست !";
-      }
-      return true;
-    },
-    validateEmail(value) {
-      if (!value) {
-        return "این فیلد الزامی می باشد !";
-      }
-      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-      if (!regex.test(value)) {
-        return "ایمیل اشتباه است !";
-      }
-      return true;
-    },
-    validatePassword(value) {
-      if (!value) {
-        return "این فیلد الزامی می باشد !";
-      }
-      const regex = /^[a-zA-Z0-9]{8,50}/;
-      if (!regex.test(value)) {
-        return "پسورد کوتاه است";
-      }
-      return true;
-    },
-  },
-};
+
+<script setup>
+import { useForm } from "vee-validate";
+import * as yup from "yup";
+
+const schema = yup.object({
+  username: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().min(8).required(),
+});
+
+const { defineField, errors, handleSubmit } = useForm({
+  validationSchema: schema,
+});
+
+const [username, usernameAttrs] = defineField("username");
+const [email, emailAttrs] = defineField("email");
+const [password, passwordAttrs] = defineField("password");
+
+const onSubmit = handleSubmit((values) => {
+  alert(JSON.stringify(values, null, 2));
+});
 </script>
