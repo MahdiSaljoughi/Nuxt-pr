925e9b73b8cd137f613fc92d565e901728d8073d
Mahdi <saljooghimahdii@gmail.com>
2024-05-28 23:33:41 +0330

    wizard

---
+++ pages/auth/api.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="email" v-model="email" placeholder="1" /><input
+      type="password"
+      v-model="password" placeholder="2"
+    />
+    <input type="submit" @click="addUser" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        email: "",
+        password: "",
+    }
+  },
+  methods: {
+    addUser() {
+      console.log("function called", this.email, this.password);
+    },
+  },
+};
+</script>
--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -3,7 +3,7 @@
     <Title>ورود / ثبت نام</Title>
   </Head>
   <div
-    class="mt-[200px] md:mt-[300px] md:mb-[50px] flex justify-center items-center"
+    class="mt-[140px] md:mt-[200px] md:mb-[50px] flex justify-center items-center"
   >
     <img
       src="/public/mobit.svg"
@@ -14,49 +14,92 @@
   </div>
   <div
     id="app"
-    class="text-center md:shadow-[0px_0px_40px_-10px_rgba(0,0,0,0.2)] w-[400px] mx-auto rounded-[24px] py-5 z-40"
+    class="text-center md:shadow-[0px_0px_40px_-10px_rgba(0,0,0,0.2)] w-[400px] mx-auto rounded-[24px] pt-5 z-40"
   >
-    <Form method="POST" @submit="onSubmit" class="flex flex-col gap-y-5">
-      <input
-        v-model="username"
-        v-bind="usernameAttrs"
-        name="username"
-        type="text"
-        class="user mx-auto outline-none focus:ring-2 ring-blue-500 bg-gray-100 w-[90%] h-[45px] rounded-[16px] placeholder:text-[12px] px-4"
-        placeholder="نام کاربری"
-      />
-      <span>{{ errors.username }}</span>
-      <input
-        v-model="email"
-        v-bind="emailAttrs"
-        name="email"
-        type="email"
-        class="mx-auto outline-none focus:ring-2 ring-blue-500 bg-gray-100 w-[90%] h-[45px] rounded-[16px] placeholder:text-[12px] px-4"
-        placeholder="ایمیل"
-      />
-      <span>{{ errors.email }}</span>
-      <input
-        v-model="password"
-        v-bind="passwordAttrs"
-        name="password"
-        type="password"
-        class="mx-auto outline-none focus:ring-2 ring-blue-500 bg-gray-100 w-[90%] h-[45px] rounded-[16px] placeholder:text-[12px] px-4"
-        placeholder="پسورد"
-      />
-      <span>{{ errors.password }}</span>
-      <button
-        type="submit"
-        class="mx-auto bg-gray-100 w-[90%] h-[45px] rounded-[16px] text-[12px] font-bold text-gray-400 focus:bg-blue-600 focus:text-white"
-      >
-        ثبت نام
-      </button>
-    </Form>
+    <FormWizard color="#094899">
+      <TabContent title="ثبت نام">
+        <Form
+          action="/"
+          method="POST"
+          @submit="onSubmit"
+          class="flex flex-col gap-y-4"
+        >
+          <input
+            v-model="username"
+            v-bind="usernameAttrs"
+            name="username"
+            type="text"
+            class="user mx-auto outline-none focus:ring-2 ring-blue-500 bg-gray-100 w-[90%] h-[45px] rounded-[16px] placeholder:text-[12px] px-4"
+            placeholder="نام کاربری"
+          />
+          <span>{{ errors.username }}</span>
+          <input
+            v-model="email"
+            v-bind="emailAttrs"
+            name="email"
+            type="email"
+            class="mx-auto outline-none focus:ring-2 ring-blue-500 bg-gray-100 w-[90%] h-[45px] rounded-[16px] placeholder:text-[12px] px-4"
+            placeholder="ایمیل"
+          />
+          <span>{{ errors.email }}</span>
+          <input
+            v-model="password"
+            v-bind="passwordAttrs"
+            name="password"
+            type="password"
+            class="mx-auto outline-none focus:ring-2 ring-blue-500 bg-gray-100 w-[90%] h-[45px] rounded-[16px] placeholder:text-[12px] px-4"
+            placeholder="پسورد"
+          />
+          <span>{{ errors.password }}</span>
+          <button
+            @click="aUser"
+            type="submit"
+            class="mx-auto mb-6 bg-gray-100 w-[90%] h-[45px] rounded-[16px] text-[12px] font-bold text-gray-400 focus:bg-blue-600 focus:text-white"
+          >
+            ثبت نام
+          </button>
+        </Form>
+      </TabContent>
+      <TabContent title="ورود">
+        <Form
+          action="/"
+          method="POST"
+          @submit="onSubmit"
+          class="flex flex-col gap-y-4"
+        >
+          <input
+            v-model="username"
+            v-bind="usernameAttrs"
+            name="username"
+            type="text"
+            class="user mx-auto outline-none focus:ring-2 ring-blue-500 bg-gray-100 w-[90%] h-[45px] rounded-[16px] placeholder:text-[12px] px-4"
+            placeholder="نام کاربری"
+          />
+          <span>{{ errors.username }}</span>
+          <input
+            v-model="password"
+            v-bind="passwordAttrs"
+            name="password"
+            type="password"
+            class="mx-auto outline-none focus:ring-2 ring-blue-500 bg-gray-100 w-[90%] h-[45px] rounded-[16px] placeholder:text-[12px] px-4"
+            placeholder="پسورد"
+          />
+          <span>{{ errors.password }}</span>
+          <button
+            type="submit"
+            class="mx-auto mb-6 bg-gray-100 w-[90%] h-[45px] rounded-[16px] text-[12px] font-bold text-gray-400 focus:bg-blue-600 focus:text-white"
+          >
+            ورود
+          </button>
+        </Form>
+      </TabContent>
+    </FormWizard>
   </div>
   <div>
     <img
       src="/public/mfoter.svg"
       class="fixed bottom-0 w-full md:hidden"
-      alt=""
+      alt="img"
     />
   </div>
 </template>
@@ -77,6 +120,11 @@ span {
 <script setup>
 import { useForm } from "vee-validate";
 import * as yup from "yup";
+import { FormWizard, TabContent } from "vue3-form-wizard";
+import "vue3-form-wizard/dist/style.css";
+components: {
+  FormWizard, TabContent;
+}
 
 const schema = yup.object({
   username: yup.string().required(),
@@ -92,7 +140,20 @@ const [username, usernameAttrs] = defineField("username");
 const [email, emailAttrs] = defineField("email");
 const [password, passwordAttrs] = defineField("password");
 
-const onSubmit = handleSubmit((values) => {
-  alert(JSON.stringify(values, null, 2));
-});
+// const addUser = await $fetch(
+//   "https://jsonplaceholder.typicode.com/todos/1/users",
+//   {
+//     method: "POST",
+//     body: JSON.stringify({
+//       username: "username",
+//       email: "email",
+//       password: "password",
+//     }),
+//   }
+// );
+// console.log(addUser);
+
+// const onSubmit = handleSubmit((values) => {
+//   alert(JSON.stringify(values, null, 2));
+// });
 </script>
